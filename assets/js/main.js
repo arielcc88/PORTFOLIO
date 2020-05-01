b5c12b4df8f28e8a1e36eeddcf720ff2bc228a90
Ariel Cuesta <arielcc88@gmail.com>
2020-04-30 22:57:14 -0500

    initial upload of portfolio project. nav bar and home working for large res viewports.

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//ACP Supporting scripts
+$(document).ready(function(){
+	//changing class of navbar on scroll.
+	$(window).scroll(function () {
+        // set distance user needs to scroll before we fadeIn navbar
+		if ($(this).scrollTop() > 0) {
+			$("#navbar-fixed-top").addClass("navbar-drop-shadow");
+			//console.log("pos", $(this).scrollTop());
+		} else {
+			$("#navbar-fixed-top").removeClass("navbar-drop-shadow");	
+		}
+	});
+});
